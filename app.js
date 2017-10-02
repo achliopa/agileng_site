c7d42e43f3ec6393178fa6a867ddb6fd69f2c4ae
Sakis Chliopanos <athchliopanos@gmail.com>
2017-10-02 06:19:56 +0000

    page link fix in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
-    res.render('index');
+    res.render('home');
 });
 
 // START APPLICATION
