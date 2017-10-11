18f523aebe978f6004e924f0d091c705c0ecbf8c
Sakis Chliopanos <athchliopanos@gmail.com>
2017-10-11 13:08:40 +0000

    added sticky navi trying to add scroll

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -5,4 +5,25 @@ function initMap() {
     zoom: 12
   });
 }
+
+var waypoint = new Waypoint({
+  element: document.querySelector('.js--section-services'),
+  handler: function(direction) {
+    if (direction === 'down') {
+      document.querySelector('nav').classList.add('sticky');
+    } else {
+      document.querySelector('nav').classList.remove('sticky');
+    }
+  },
+  offset: 60 
+})
+
+document.querySelector('.js--scroll-to-join').onclick = function(event) {
+  document.querySelector('.section-form').scrollIntoView(true);
+}
+
+// document.querySelector('.js--scroll-to-project')
+// document.querySelector('.js--scroll-to-services')
+// document.querySelector('.js--scroll-to-contact')
+
 // AIzaSyD19bKfrPlfqkMEIZClW2dWf0IMChyOOS4
